a5cb2e24994c5bd990789b91d2ec02490ec431d9
Jonatas-Arao-Estudos <jonatas12apostolico@hotmail.com>
2019-11-03 02:55:54 +0900

    Telas Cadastro e Login

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,20 @@
-// This is a JavaScript file
+$(document).on('click','#paraCadastro',function(){
+  var email = $('#email').val();
+  var senha = $('#senha').val();
+  sessionStorage.setItem('preCadastroEmail', email);
+  sessionStorage.setItem('preCadastroSenha', senha);
+  window.location.href = "cadastro.html";
+});
 
+$(document).on('submit','#frmUsuario',function(){
+	var dados = $(this).serialize();
+	$.ajax({
+		url: 'https://atividadeappcrudinho.000webhostapp.com/controlUsuario.php?acao=cadastrar',
+		type: 'POST',
+		data: dados,
+		success:function(retorno){
+			navigator.notification.alert(retorno);
+		}
+	});
+	return false;
+});
